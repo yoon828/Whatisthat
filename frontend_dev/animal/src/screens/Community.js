5545f3c6ea29f08e6d021eedeeafa18063d3d911
고요한 <rdg9074@naver.com>
2022-09-07 09:56:11 +0900

    Frontend dev

---
+++ frontend_dev/animal/src/screens/Community.js
@@ -0,0 +1,11 @@
+
+
+const CommunityPage = () => {
+    return (
+        <div>
+            <p>커뮤니티</p>
+        </div>
+    )
+}
+
+export default CommunityPage;
---
+++ frontend_dev/animal/src/screens/Facilities.js
@@ -0,0 +1,11 @@
+
+
+const FacilitiesPage = () => {
+    return (
+        <div>
+            주변시설
+        </div>
+    )
+}
+
+export default FacilitiesPage;
